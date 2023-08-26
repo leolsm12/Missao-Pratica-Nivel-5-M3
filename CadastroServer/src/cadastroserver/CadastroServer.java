a78cdc592a37adf87b90c275e57ea3d76b40cf47
Leonardo Schaffer <leoschaffer@hotmail.com>
2023-08-25 21:16:34 -0300

    teste

---
+++ CadastroServer/src/cadastroserver/CadastroServer.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastroserver;
+
+import controller.ProdutosJpaController;
+import controller.UsuariosJpaController;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+/**
+ *
+ * @author leosc
+ */
+public class CadastroServer {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("CadastroServerPU");
+        ProdutosJpaController ctrl = new ProdutosJpaController(emf);
+        UsuariosJpaController ctrlUsu = new UsuariosJpaController(emf);
+
+        try (ServerSocket serverSocket = new ServerSocket(4321)) {
+            System.out.println("Servidor aguardando conexoes na porta 4321...");
+            
+            while (true) {
+                Socket socket = serverSocket.accept();
+                CadastroThread thread = new CadastroThread(ctrl, ctrlUsu, socket);
+                thread.start(); // Inicia a thread
+                System.out.println("thread iniciado!");
+            }
+            
+        }
+    }
+}
+   
+    
+
